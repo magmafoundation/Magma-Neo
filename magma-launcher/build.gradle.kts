7aeb708fdc7a0ca9396c01c9e9438d22ee52dd24
Hexeption <minecraftfun201@gmail.com>
2025-05-25 05:42:11 +0100

    feat: start of launcher

---
+++ magma-launcher/build.gradle.kts
@@ -0,0 +1,42 @@
+import java.util.Calendar
+
+plugins {
+    id("java-library")
+    id("net.neoforged.licenser")
+    id("neoforge.formatting-conventions")
+    kotlin("jvm")
+}
+
+
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly(platform("net.neoforged:minecraft-dependencies:${project.property("minecraft_version")}"))
+    compileOnly("org.slf4j:slf4j-api")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+license {
+    header(rootProject.file("codeformat/MAGMA-HEADER.txt"))
+    properties {
+        this["year"] = Calendar.getInstance().get(Calendar.YEAR).toString()
+    }
+    include("**/*.java", "**/*.kt")
+}
+
+tasks.jar.configure {
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
---
+++ magma-launcher/src/main/kotlin/org/magmafoundation/magma/launcher/MagmaLauncher.kt
@@ -0,0 +1,50 @@
+/*
+ * Magma Server
+ * Copyright (C) 2019-2025.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.magmafoundation.magma.launcher
+
+import java.util.jar.Manifest
+
+
+var ui: UI? = null
+
+fun main(args: Array<String>) {
+
+    ui = UI(!isArgumentPresent(args, "--no-ui"))
+
+    val version = getVersion()
+
+    ui?.display(version = version, neoForgeVersion = getNeoForgeVersion())
+}
+
+private fun isArgumentPresent(args: Array<String>, argument: String): Boolean {
+    return args.any { it.equals(argument, ignoreCase = true) }
+}
+
+fun getVersion(): String {
+    val version = object {}.javaClass.`package`?.implementationVersion
+    return version ?: "dev-env"
+}
+
+fun getNeoForgeVersion(): String {
+    val neoForgeVersion = object {}.javaClass.classLoader.getResourceAsStream("META-INF/MANIFEST.MF")?.use { inputStream ->
+        val manifest = Manifest(inputStream)
+        manifest.mainAttributes.getValue("NeoForge-Version")
+    }
+    return neoForgeVersion ?: "dev-env"
+}
