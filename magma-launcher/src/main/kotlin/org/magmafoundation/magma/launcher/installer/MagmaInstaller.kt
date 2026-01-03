66ed471149234beecdd4d02e9ae8c37bf8451d60
Keir Davis <admin@hexeption.co.uk>
2026-01-03 19:27:47 +0000

    chore: update license year

--- magma-launcher/src/main/kotlin/org/magmafoundation/magma/launcher/installer/MagmaInstaller.kt
+++ magma-launcher/src/main/kotlin/org/magmafoundation/magma/launcher/installer/MagmaInstaller.kt
@@ -1,6 +1,6 @@
 /*
  * Magma Server
- * Copyright (C) 2019-2025.
+ * Copyright (C) 2019-2026.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
